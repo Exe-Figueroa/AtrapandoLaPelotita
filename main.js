4f0e36bbe84c03550fa6feaaa17410f9a17f58fc
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-02-23 18:01:09 -0300

    coso del coso

--- main.js
+++ main.js
@@ -8,10 +8,40 @@ let height = miDiv.getBoundingClientRect().height;
 
 function moverla() {
 
-    console.log('holaa')
     
     let min = 50
 
-   pelotita.style.left = (Math.random() * (width - 70) ) + "px";
-   pelotita.style.top = (Math.random() * (height - 70) ) + "px";
+    pelotita.style.left = (Math.random() * (width - 70) ) + "px";
+    pelotita.style.top = (Math.random() * (height - 70) ) + "px";
+    indicador++
+    switch(indicador){
+        case 0:
+            alert('osoooo')
+        break;
+        case 1: 
+        alert('casii')
+        break
+        case 2:
+        console.log(indicador)
+        break;
+        case 3: 
+        console.log(indicador)
+        break
+        case 4:
+        console.log(indicador)
+        break;
+        case 5: 
+        console.log(indicador)
+        break
+        case 6:
+        console.log(indicador)
+        break;
+        case 7: 
+        console.log(indicador)
+        break
+        default: 
+        indicador=0
+
+    }
+    
 }
