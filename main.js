32b2fca45f6786fb615fa9c7ac75106bc892cf7f
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-02-24 17:31:39 -0300

    En proceso

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 
-const pelotita = document.querySelector('.pelotita-container')
+const pelotitac = document.querySelector('.pelotita-container')
 const miDiv = document.querySelector(".move-ball");
 let indicador=0
-
-pelotita.addEventListener('mouseenter', moverla)
+const containGame = document.querySelector ('.move-ball-container')
+const btnStart = document.querySelector ('.play-game-button')
+btnStart.addEventListener('click', playGame)
+pelotitac.addEventListener('mouseenter', moverla)
 
 let width = miDiv.getBoundingClientRect().width;
 let height = miDiv.getBoundingClientRect().height;
@@ -15,37 +17,41 @@ function moverla() {
     
     let min = 50
 
-    pelotita.style.left = (Math.random() * (width - 70) ) + "px";
-    pelotita.style.top = (Math.random() * (height - 70) ) + "px";
-   
+    pelotitac.style.left = (Math.random() * (width - 70) ) + "px";
+    pelotitac.style.top = (Math.random() * (height - 70) ) + "px";
     switch(indicador){
         case 0:
-            console.log(0)
+            console.log(indicador)
+            cartelito.innerHTML = 'Epaaa'
         break;
         case 1: 
             cartelito.innerHTML = 'casii'
         break
         case 2:
         console.log(indicador)
+        cartelito.innerHTML = 'osoo'
         break;
         case 3: 
         console.log(indicador)
+        cartelito.innerHTML = 'Fallaste'
         break
         case 4:
         console.log(indicador)
+        cartelito.innerHTML = 'Cachulelu no me atrapa'
+        cartelito.style.marginLeft='70px'
         break;
         case 5: 
         console.log(indicador)
         break
         case 6:
-        console.log(indicador)
+        cartelito.innerHTML = 'oleeee'
         break;
         case 7: 
         console.log(indicador)
         break
         default: 
         indicador=0
-
     }
     indicador++
+
 }
