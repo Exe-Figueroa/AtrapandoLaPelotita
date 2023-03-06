a4e00e149eb52ca4883fc267fd3ebfac92e93b65
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-03-06 14:54:09 -0300

    Pelotita con movimiento constante y con un intervalo de 230ms

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const btnStart = document.querySelector ('.play-game-button')
 
 
 btnStart.addEventListener('click', playGame)
-pelotitac.addEventListener('mouseenter', moverla)
+
 pelotitac.addEventListener('click', ganaste)
     
 let width = miDiv.getBoundingClientRect().width;
@@ -25,47 +25,51 @@ function playGame(){
     containGame.classList.remove('inactive');
     btnStart.classList.add('inactive');
     obtenerTamañoContenedor();
+    setInterval(moverla, 230)
 };
+
 function moverla() {
     pelotitac.style.left = (Math.random() * (width - 70) ) + "px";
     pelotitac.style.top = (Math.random() * (height - 70) ) + "px";
-    switch(indicador){
-        case 0:
-            console.log(indicador)
-            cartelito.innerHTML = 'Epaaa'
-        break;
-        case 1: 
-            cartelito.innerHTML = 'casii'
-        break
-        case 2:
-        console.log(indicador)
-        cartelito.innerHTML = 'osoo'
-        break;
-        case 3: 
-        console.log(indicador)
-        cartelito.innerHTML = 'Fallaste'
-        break
-        case 4:
-        console.log(indicador)
-        cartelito.innerHTML = 'Cachulelu no me atrapa'
-        cartelito.style.marginLeft='70px'
-        break;
-        case 5: 
-        console.log(indicador)
-        break
-        case 6:
-        cartelito.innerHTML = 'oleeee'
-        break;
-        case 7: 
-        console.log(indicador)
-        break
-        default: 
-        indicador=0
-    }
-    indicador++
-
 }
 
 function ganaste(){
     alert('ganaste')
 }
+const osooo=()=>{
+switch(indicador){
+    case 0:
+        console.log(indicador)
+        cartelito.innerHTML = 'Epaaa'
+    break;
+    case 1: 
+        cartelito.innerHTML = 'casii'
+    break
+    case 2:
+    console.log(indicador)
+    cartelito.innerHTML = 'osoo'
+    break;
+    case 3: 
+    console.log(indicador)
+    cartelito.innerHTML = 'Fallaste'
+    break
+    case 4:
+    console.log(indicador)
+    cartelito.innerHTML = 'Cachulelu no me atrapa'
+    cartelito.style.marginLeft='70px'
+    break;
+    case 5: 
+    console.log(indicador)
+    break
+    case 6:
+    cartelito.innerHTML = 'oleeee'
+    break;
+    case 7: 
+    console.log(indicador)
+    break
+    default: 
+    indicador=0
+}
+indicador++
+}
+miDiv.addEventListener('click', osooo)
